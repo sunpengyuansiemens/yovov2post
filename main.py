2232e66d8d05757d57db8199fb5d30d0f167036d
Rao Xin <xin.rao@siemens.com>
2023-02-27 16:56:08 +0800

    sort prediction result

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import numpy as np
 import cv2
 import onnxruntime
 import math
+import copy
 
 model = "./tinyyolov2-8.onnx"
-path = "./test.JPG"
+path = "./1.JPG"
 
 # Preprocess the image
 img = cv2.imread(path)
-img = cv2.resize(img, dsize=(416, 416), interpolation=cv2.INTER_AREA)
-img.resize((1, 3, 416, 416))
+img = cv2.resize(img, dsize=(416, 416))
+img = img.swapaxes(0, 2)
+img = np.expand_dims(img, axis=0)
 
 data = json.dumps({'data': img.tolist()})
 data = np.array(json.loads(data)['data']).astype('float32')
@@ -26,8 +28,21 @@ anchors = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843,
            5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
 
 
-def compute_bounding_boxes(model_result):
-    boxes = []
+class Prediction:
+    def __init__(self, x, y, w, h, c, i):
+        self.x = x - w / 2
+        self.y = y - h / 2
+        self.width = w
+        self.height = h
+        self.confidence = c
+        self.index = i
+
+    def getConfidence(self):
+        return self.confidence
+
+
+def compute_prediction_list(model_result):
+    predictions = []
 
     for cy in range(13):
         for cx in range(13):
@@ -42,30 +57,36 @@ def compute_bounding_boxes(model_result):
                 # apply sigmoid function to raw data
                 x = cx + sigmoid(tx) * 32
                 y = cy + sigmoid(ty) * 32
-                w = math.exp(tw) * anchors[2*b] * 32
-                h = math.exp(th) * anchors[2*b + 1] * 32
+                w = math.exp(tw) * anchors[2 * b] * 32
+                h = math.exp(th) * anchors[2 * b + 1] * 32
                 confidence = sigmoid(tc)
 
                 classes = [0 for i in range(num_classes)]
                 for c in range(num_classes):
                     classes[c] = model_result[0][0][channel + 5 + c][cx][cy]
 
-                detectedClass, bestClassScore = softmax(classes)
-
+                originalClass = copy.deepcopy(classes)
+                bestClassScore = getMax(classes)
+                detectedClass = originalClass.index(bestClassScore)
                 confidenceInClass = bestClassScore * confidence
+                if confidenceInClass < 0.3:
+                    continue
+                predictions.append(Prediction(x, y, w, h, confidenceInClass, detectedClass))
 
-    return boxes
-
-
-def softmax(classes):
-    temp = np.exp(classes) / np.sum(np.exp(classes))
-    return np.argmax(temp), np.max(temp)
+    return predictions
 
 
-def sigmoid(value:float):
-    return 1/(1 + math.exp(-value))
+def getMax(classes):
+    classes.sort(reverse=True)
+    return classes[0]
 
 
-r = compute_bounding_boxes(result)
+def sigmoid(value: float):
+    return 1 / (1 + math.exp(-value))
+
+
+lst = compute_prediction_list(result)
+
+lst.sort(key=lambda a: a.getConfidence(), reverse=True)
 
 print()
